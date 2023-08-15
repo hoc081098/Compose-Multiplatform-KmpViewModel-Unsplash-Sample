95740415765d9172aa14995a1700cde747f7af4f
hoc081098@gmail.com <hoc081098@gmail.com>
2023-08-16 01:33:55 +0700

    done dsl with suspend

--- common_shared/src/commonMain/kotlin/com/hoc081098/compose_multiplatform_kmpviewmodel_sample/common_shared/publishWithSelector.kt
+++ common_shared/src/commonMain/kotlin/com/hoc081098/compose_multiplatform_kmpviewmodel_sample/common_shared/publishWithSelector.kt
@@ -4,6 +4,7 @@ import com.hoc081098.flowext.defer
 import com.hoc081098.flowext.interval
 import com.hoc081098.flowext.takeUntil
 import com.hoc081098.flowext.timer
+import com.hoc081098.flowext.withLatestFrom
 import kotlin.concurrent.Volatile
 import kotlin.jvm.JvmField
 import kotlinx.coroutines.CancellationException
@@ -50,10 +51,12 @@ sealed interface SelectorSharedFlowScope<T> {
   ): SharedFlow<T> = throw UnsupportedOperationException("Not implemented, should not be called")
 }
 
+typealias SelectorFunction<T, R> = suspend SelectorSharedFlowScope<T>.(Flow<T>) -> Flow<R>
+
 @PublishSelectorDsl
 sealed interface SelectorScope<T> {
   @PublishSelectorDsl
-  fun <R> select(block: SelectorSharedFlowScope<T>.(Flow<T>) -> Flow<R>): Flow<R>
+  fun <R> select(block: SelectorFunction<T, R>): Flow<R>
 }
 
 @OptIn(DelicateCoroutinesApi::class)
@@ -64,10 +67,13 @@ private class DefaultSelectorScope<T>(
   // Guarded by mutex
   private lateinit var channels: Array<Channel<T>>
   private lateinit var selectorFlows: Array<Flow<T>>
-  private lateinit var cachedSelectedFlows: Array<Flow<Any?>?>
+  private lateinit var cachedOutputFlows: Array<Flow<Any?>?>
 
   @JvmField
-  val blocks: MutableList<SelectorSharedFlowScope<T>.(Flow<T>) -> Flow<Any?>> = ArrayList()
+  val mutex = Mutex()
+
+  @JvmField
+  val blocks: MutableList<SelectorFunction<T, Any?>> = ArrayList()
 
   @Volatile
   @JvmField
@@ -77,10 +83,7 @@ private class DefaultSelectorScope<T>(
   @JvmField
   var isInSelectClause = false
 
-  @JvmField
-  val mutex = Mutex()
-
-  override fun <R> select(block: SelectorSharedFlowScope<T>.(Flow<T>) -> Flow<R>): Flow<R> {
+  override fun <R> select(block: SelectorFunction<T, R>): Flow<R> {
     check(!isInSelectClause) { "select can not be called inside another select" }
     check(!isFrozen) { "select only can be called inside publish, do not use SelectorScope outside publish" }
 
@@ -90,13 +93,17 @@ private class DefaultSelectorScope<T>(
     val index = blocks.size - 1
 
     return defer {
+      // Only frozen state can reach here,
+      // that means we collect the output flow after frozen this scope
+      check(isFrozen) { "only frozen state can reach here!" }
+
       val outputFlow = mutex.withLock {
-        cachedSelectedFlows[index]
-          ?: block(selectorFlows[index])
-            .also { cachedSelectedFlows[index] = it }
+        cachedOutputFlows[index]
+          ?: block(this@DefaultSelectorScope, selectorFlows[index])
+            .also { cachedOutputFlows[index] = it }
       }
 
-      @Suppress("UNCHECKED_CAST")
+      @Suppress("UNCHECKED_CAST") // Always safe
       outputFlow as Flow<R>
     }.also { isInSelectClause = false }
   }
@@ -113,7 +120,7 @@ private class DefaultSelectorScope<T>(
 
       channels = Array(size) { Channel() }
       selectorFlows = Array(size) { channels[it].consumeAsFlow() }
-      cachedSelectedFlows = Array(size) { null }
+      cachedOutputFlows = Array(size) { null }
     }
   }
 
@@ -146,13 +153,16 @@ private class DefaultSelectorScope<T>(
   }
 }
 
-fun <T, R> Flow<T>.publish(selector: SelectorScope<T>.() -> Flow<R>): Flow<R> {
+fun <T, R> Flow<T>.publish(selector: suspend SelectorScope<T>.() -> Flow<R>): Flow<R> {
   val source = this
 
   return flow {
     coroutineScope {
       val scope = DefaultSelectorScope<T>(this)
+
       val output = selector(scope)
+
+      // IMPORTANT: freeze and init before collect the output flow
       scope.freezeAndInit()
 
       launch {
@@ -170,6 +180,7 @@ fun <T, R> Flow<T>.publish(selector: SelectorScope<T>.() -> Flow<R>): Flow<R> {
         }
       }
 
+      // IMPORTANT: collect the output flow after frozen the scope
       emitAll(output)
     }
   }
@@ -178,6 +189,7 @@ fun <T, R> Flow<T>.publish(selector: SelectorScope<T>.() -> Flow<R>): Flow<R> {
 suspend fun main() {
   flow<Any?> {
     println("Collect...")
+    delay(100)
     emit(1)
     delay(100)
     emit(2)
@@ -187,17 +199,20 @@ suspend fun main() {
     emit("4")
   }
     .publish {
+      delay(100)
+
       merge(
-        select {
-          val sharedFlow = it.shared()
+        select { flow ->
+          delay(1)
+          val sharedFlow = flow.shared()
 
           interval(0, 100)
             .takeUntil(sharedFlow.filter { it == 3 })
             .onEach { println(">>> interval: $it") }
             .flatMapLatest { value ->
-              timer(0, 50)
-                .takeUntil(sharedFlow)
-                .map { value to "shared" }
+              timer(value, 50)
+                .withLatestFrom(sharedFlow)
+                .map { it to "shared" }
             }
         },
         select { flow ->
