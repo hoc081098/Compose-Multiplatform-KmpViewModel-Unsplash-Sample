b0237eb8cb200808294e46a57fb9321dcd23e34b
hoc081098@gmail.com <hoc081098@gmail.com>
2023-08-24 17:10:45 +0700

    [common] add ":libraries:coroutines-utils"

--- feature_search_photo_shared/src/commonMain/kotlin/com/hoc081098/compose_multiplatform_kmpviewmodel_sample/search_photo/data/RealSearchPhotoRepository.kt
+++ feature_search_photo_shared/src/commonMain/kotlin/com/hoc081098/compose_multiplatform_kmpviewmodel_sample/search_photo/data/RealSearchPhotoRepository.kt
@@ -1,9 +1,11 @@
 package com.hoc081098.compose_multiplatform_kmpviewmodel_sample.search_photo.data
 
 import arrow.core.Either
+import com.hoc081098.compose_multiplatform_kmpviewmodel_sample.coroutines_utils.AppCoroutineDispatchers
 import com.hoc081098.compose_multiplatform_kmpviewmodel_sample.search_photo.data.response.CoverPhotoResponse
 import com.hoc081098.compose_multiplatform_kmpviewmodel_sample.search_photo.domain.CoverPhoto
 import com.hoc081098.compose_multiplatform_kmpviewmodel_sample.search_photo.domain.SearchPhotoRepository
+import kotlinx.coroutines.withContext
 import org.koin.core.annotation.Singleton
 
 @Singleton(
@@ -14,13 +16,18 @@ import org.koin.core.annotation.Singleton
 internal class RealSearchPhotoRepository(
   private val unsplashApi: UnsplashApi,
   private val searchPhotoErrorMapper: SearchPhotoErrorMapper,
+  private val appCoroutineDispatchers: AppCoroutineDispatchers,
 ) : SearchPhotoRepository {
-  override suspend fun search(query: String) = Either.catch {
-    unsplashApi
-      .searchPhotos(query)
-      .results
-      .map(CoverPhotoResponse::toCoverPhoto)
-  }.mapLeft(searchPhotoErrorMapper)
+  override suspend fun search(query: String) = withContext(appCoroutineDispatchers.io) {
+    Either
+      .catch {
+        unsplashApi
+          .searchPhotos(query)
+          .results
+          .map(CoverPhotoResponse::toCoverPhoto)
+      }
+      .mapLeft(searchPhotoErrorMapper)
+  }
 }
 
 private fun CoverPhotoResponse.toCoverPhoto() = CoverPhoto(
