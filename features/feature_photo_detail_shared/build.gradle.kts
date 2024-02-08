e2c4927fee6c1ec943148359b4d64f1051bb9949
hoc081098@gmail.com <hoc081098@gmail.com>
2024-02-08 14:26:52 +0700

    refactor gradle

--- features/feature_photo_detail_shared/build.gradle.kts
+++ features/feature_photo_detail_shared/build.gradle.kts
@@ -40,8 +40,10 @@ kotlin {
   iosArm64()
   iosSimulatorArm64()
 
+  applyDefaultHierarchyTemplate()
+
   sourceSets {
-    val commonMain by getting {
+    commonMain{
       kotlin.srcDir("build/generated/ksp/metadata/commonMain/kotlin")
 
       dependencies {
@@ -115,7 +117,7 @@ kotlin {
         api(libs.napier)
       }
     }
-    val androidMain by getting {
+    androidMain  {
       dependencies {
         api(libs.androidx.activity.compose)
         api(libs.androidx.appcompat)
@@ -128,15 +130,7 @@ kotlin {
         implementation(libs.kotlinx.coroutines.android)
       }
     }
-    val iosX64Main by getting
-    val iosArm64Main by getting
-    val iosSimulatorArm64Main by getting
-    val iosMain by creating {
-      dependsOn(commonMain)
-      iosX64Main.dependsOn(this)
-      iosArm64Main.dependsOn(this)
-      iosSimulatorArm64Main.dependsOn(this)
-
+    iosMain {
       dependencies {
         // Ktor
         implementation(libs.ktor.client.darwin)
